cf84b04bc5c4746dcd71057a536c552012605aba
IhitplayzYT <ihitexe@gmail.com>
2026-06-08 22:57:55 +0530

    Finished creation of BK Tree and added Optimised id mapped BK trees alongside expansion ofg some more future updates

--- src/helper.rs
+++ src/helper.rs
@@ -3,9 +3,9 @@ pub mod Helper{
     use std::{path::Path, vec};
 
 
-const DBG_STR:&str = "";
-const OK: i32 = 0;
-const ERR: i32 = 1;
+pub const DBG_STR:&str = "";
+pub const OK: i32 = 0;
+pub const ERR: i32 = 1;
 // Just a spce seperated or new line based custom file from which we'll extract our vocabulary
 const DEFAULT_DICTIONARY: &str = "dictionary/en-dict.dict";
 
@@ -19,7 +19,9 @@ std::process::exit(OK);
 pub struct CLI {
 pub dict_file: Vec<String>,
 pub proc_id: Option<i32>,
-pub dbg: bool
+pub dbg: bool,
+pub optimise: bool,
+pub learn: bool
 }
 
 impl CLI {
@@ -27,32 +29,23 @@ impl CLI {
         Self{
             dict_file: d_vec,
             proc_id:pid,
-            dbg: false
+            dbg: false,
+            optimise: false,
+            learn:false
         }
     }
 }
 
-pub fn extract_vocab(fpath: String) -> Vec<String> {
+pub fn extract_vocab(fpath: &Path) -> Vec<String> {
 let mut ret = vec![];
-if Path::is_dir(Path::new(&fpath[..])) {
-    // Entire dir contains our dictionary
-    let dir = std::fs::read_dir(fpath);
-    for i in &dir{
-
-        // TODO: FIXME:
+if fpath.is_dir() {
+    for i in  std::fs::read_dir(&fpath).unwrap().flatten(){
+        ret.append(&mut extract_vocab(&i.path()));
     }
-}else{
-    let vocab = std::fs::read(fpath).unwrap_or_else(|_| { std::process::exit(ERR)});
-    let mut word = "".to_string();
-    for i in vocab{
-        if i != b' ' || i != b'\n'{
-            word += &i.to_string()[..];
-        }else{
-            ret.push(word.clone());
-            word.clear();
-        }
-    }
-
+}else if fpath.ends_with(".dict") || fpath.ends_with(".txt") {
+    let bytes = std::fs::read(fpath).unwrap_or_else(|_| { std::process::exit(ERR)});
+    let file = String::from_utf8(bytes).unwrap();
+    ret.append(&mut String::from_utf8(file.bytes().map(|ch| if ch == b'\n' {b' '} else {ch}).collect::<Vec<u8>>()).unwrap().split_ascii_whitespace().into_iter().map(|x| x.to_string()).collect::<Vec<String>>());
 }
     ret
 }
@@ -69,6 +62,15 @@ let args = std::env::args().collect::<Vec<String>>();
         }
         else if i.as_str() == "-d" || i.as_str() == "--debug" || i.as_str() == "-D" || i.as_str() == "--DEBUG"{
             ret.dbg = true;
+        }
+        else if i.as_str() == "-o" || i.as_str() == "--optimise" || i.as_str() == "-O" || i.as_str() == "--OPTIMISE"{
+            ret.optimise = true;
+        }
+        else if i.as_str() == "-l" || i.as_str() == "--learn" || i.as_str() == "-L" || i.as_str() == "--LEARN"{
+            ret.learn = true;
+        }
+        else if i.as_str().starts_with("-p="){
+            ret.proc_id = Some(i[3..].parse::<i32>().expect("Expected a valid non negative pid"))
         }
 
         if let Ok(ID) = i.parse::<i32>(){
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![allow(non_snake_case,non_camel_case_types,dead_code)]
 
 mod helper;
-use std::sync::atomic;
+mod speru;
 
+use std::path::Path;
+use speru::Speru;
 use helper::Helper;
 fn main() {
 let clargs = Helper::Parse_args();
@@ -12,7 +14,16 @@ if clargs.dbg {
 }
 
 for i in &clargs.dict_file {
-       wordlist.append(&mut Helper::extract_vocab(i.to_string()));
+    wordlist.append(&mut Helper::extract_vocab(Path::new(i)));
+}
+
+if clargs.optimise{
+    wordlist.iter().for_each(|x| if !Speru::set_word(x) {std::process::exit(Helper::ERR)});
+    let id_list = Speru::Id2Str_Map.read().unwrap().clone().into_keys().collect::<Vec<usize>>();
+    let root = Speru::O_BK_Tree::from_vec(id_list); 
+}else{
+    wordlist.iter().for_each(|x| if !Speru::set_word(x) {std::process::exit(Helper::ERR)});
+    let root = Speru::BK_Tree::from_vec(wordlist); 
 }
 
 if let Some(pid) = clargs.proc_id {
